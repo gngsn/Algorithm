8e3dc39ae09a436dc93c3262d53769c3bd079b17
gngsn <rudtjs4540@gmail.com>
2020-05-15 23:52:22 +0900

    update solved baekjoon problem

---
+++ src/BaekJoon10162.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class BaekJoon10162 {
+    public static void main(String[] args) {
+        Integer[] button = {300, 60, 10};
+        String answer;
+        Scanner sc = new Scanner(System.in);
+        String timeStr =  sc.nextLine();
+        Integer time = Integer.parseInt(timeStr);
+        answer = time / button[0] + " ";
+        time %= button[0];
+        answer += time / button[1] + " ";
+        time %= button[1];
+        answer += time / button[2];
+        time %= button[2];
+        answer = time != 0 ? "-1" : answer;
+        System.out.println(answer);
+    }
+}
---
+++ src/BaekJoon10807.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class BaekJoon10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        HashMap<String, Integer> numMap = new HashMap<>();
+        String input = sc.nextLine();
+        String[] numArray = input.split(" ");
+        for (String num : numArray) {
+            Integer n = numMap.get(num);
+            numMap.put(num, n == null ? 1 : n+1);
+        }
+        String findNum = sc.nextLine();
+        Integer answer = numMap.get(findNum);
+        System.out.println(answer == null ? 0 : answer);
+    }
+}
