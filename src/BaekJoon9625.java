6fb06eed55264c1d81d6618659df8d1d370ba929
gngsn <rudtjs4540@gmail.com>
2020-05-27 22:43:06 +0900

    Update BaekJoon9625

--- src/BaekJoon9625.java
+++ src/BaekJoon9625.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
+import java.util.HashMap;
+import java.util.Scanner;
 public class BaekJoon9625 {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        HashMap<String, Integer> ab = new HashMap<>();
+        ab.put("A", 1);
+        ab.put("B", 0);
+        while (n!=0) {
+            int a = ab.get("A");
+            int b = ab.get("B");
+            ab.put("A", b);
+            ab.put("B", b+a);
+            n--;
+        }
+        System.out.println(ab.get("A")+" "+ab.get("B"));
+    }
 }
