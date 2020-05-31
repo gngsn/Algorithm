bb077be95070afd846bce60b2e048106efcd7499
gngsn <rudtjs4540@gmail.com>
2020-05-31 15:41:42 +0900

    Update BaekJoon 11650

--- src/week3/BaekJoon11650.java
+++ src/week3/BaekJoon11650.java
@@ -1,26 +1,25 @@
 package week3;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class BaekJoon11650 {
-    private static HashMap<Integer, ArrayList<Integer>> arr = new HashMap<>();
-    private static ArrayList<Integer> tmp = new ArrayList<>();
-    private static ArrayList<Integer> src = new ArrayList<>();
     public static void main(String[] args) {
+        HashMap<Integer, ArrayList<Integer>> arr = new HashMap<>();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
             ArrayList a = arr.getOrDefault(x, new ArrayList<>());
             a.add(y);
             arr.put(x, a);
         }
-        for(Integer key : arr.keySet()) {
+        for (Integer key : arr.keySet()) {
             if (arr.get(key).size() > 1) {
-                tmp = new ArrayList<>();
-                src = arr.get(key);
-                mergeSort(0, arr.get(key).size() - 1);
+                ArrayList<Integer> src = arr.get(key);
+                mergeSort(src);
                 arr.put(key, src);
             }
             for (Integer val : arr.get(key)) {
@@ -28,24 +27,42 @@ public class BaekJoon11650 {
             }
         }
     }
-    static void mergeSort(int start, int end) {
-        if (start<end) {
-            int mid = (start+end) / 2;
-            mergeSort(start, mid);
-            mergeSort(mid+1, end);
-            int p = start;
-            int q = mid+1;
-            int idx = p;
-            while (p<=mid || q<=end) {
-                System.out.println("src : "+src);
-                System.out.println("q : "+q);
-                if (q>end || (p<=mid && src.get(p) < src.get(q))) {
-                    tmp.add(idx++, src.get(p++)) ;
-                } else {
-                    tmp.add(idx++, src.get(q++));
-                }
+
+    static void mergeSort(ArrayList arr) {
+        sort(arr, 0, arr.size());
+    }
+
+    static void sort(ArrayList arr, int low, int high) {
+        if (high - low < 2) {
+            return;
+        }
+        int mid = (low + high) / 2;
+        sort(arr, 0, mid);
+        sort(arr, mid, high);
+        merge(arr, low, mid, high);
+    }
+
+    static void merge(ArrayList<Integer> arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int t = 0, l = low, h = mid;
+
+        while (l < mid && h < high) {
+            if (arr.get(l) < arr.get(h)) {
+                temp.add(t++, arr.get(l++));
+            } else {
+                temp.add(t++, arr.get(h++));
             }
-            src = tmp;
+        }
+
+        while (l < mid) {
+            temp.add(t++, arr.get(l++));
+        }
+
+        while (h < high) {
+            temp.add(t++, arr.get(h++));
+        }
+        for (int i = low; i < high; i++) {
+            arr.set(i, temp.get(i - low));
         }
     }
 }
