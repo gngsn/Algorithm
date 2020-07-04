ff54ef23a4eb89499f32080faabf7a68d642b0d0
gngsn <rudtjs4540@gmail.com>
2020-07-04 15:47:55 +0900

    Update boostcamp Q

--- programmers/boostcamp/boostcamp01.java
+++ programmers/boostcamp/boostcamp01.java
@@ -1,16 +1,36 @@
 package boostcamp;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class boostcamp01 {
-    static String[] solution(String[] tickets) {
-        String[] answer = {};
+    static boolean solution(String[] name_list) {
+        char[] n = {};
+        for (int i=0; i<name_list.length; i++) {
+            Pattern pattern1 = Pattern.compile("(^"+name_list[i]+")*");
+            for (int j=i+1; j < name_list.length; j++) {
+                Pattern pattern2 = Pattern.compile("(^"+name_list[j]+")*");
+                Matcher m = pattern1.matcher(name_list[j]);
+                if (m.find())
+                    if (m.group(1) != null)
+                        return true;
 
-        return answer;
+                m = pattern2.matcher(name_list[i]);
+                if (m.find())
+                    if (m.group(1) != null)
+                        return true;
+            }
+        }
+        return false;
     }
 
 
-    static public void main(String[] args) {
-        String[] tickets = {};
-        String[] tickets2;
-        solution(tickets);
+    public static void main(String args[]) {
+        String[] animal = {"가을", "우주", "너굴"};
+        String[] animal2 = {"봄", "여울", "봄봄"};
+        String[] animal4 = {"봄봄", "여울", "봄"};
+        String[] animal3 = {"너굴", "너울", "여울", "서울"};
+        System.out.println(solution(animal3));
     }
+
 }
--- programmers/boostcamp/boostcamp02.java
+++ programmers/boostcamp/boostcamp02.java
@@ -1,16 +1,72 @@
 package boostcamp;
+import java.util.*;
 
 public class boostcamp02 {
-    static String[] solution(String[] tickets) {
-        String[] answer = {};
+    static int[] solution(int[] arrayA, int[] arrayB) {
+        int[] answer = new int[5];
+
+        List<Integer> a = makeList(arrayA);
+        List<Integer> b = makeList(arrayB);
+
+        Set<Integer> ah = new HashSet();
+        ah.addAll(a);
+        Set<Integer> bh = new HashSet();
+        bh.addAll(b);
+
+        Set<Integer> s = sum(a, b);
+        List<Integer> c = complement(a, b);
+        List<Integer> i = intersaction(a, b);
+
+        answer[0] = a.size();
+        answer[1] = b.size();
+        answer[2] = s.size();
+        answer[3] = c.size();
+        answer[4] = i.size();
 
         return answer;
     }
 
+    static List<Integer> complement(List<Integer> base, List<Integer> other) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int i = 0;
+        for(int b : base) {
+            if(!other.contains(b) && !arr.contains(b)) {
+                arr.add(i++, b);
+            }
+        }
+        return arr;
+    }
+
+    static ArrayList<Integer> intersaction(List<Integer> base, List<Integer> other) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int i = 0;
+        for(int b : base) {
+            if(other.contains(b) && !arr.contains(b)) {
+                arr.add(i++, b);
+            }
+        }
+        return arr;
+    }
+
+    static Set<Integer> sum(List<Integer> base, List<Integer> other) {
+        Set<Integer> s = new HashSet();
+        s.addAll(base);
+        s.addAll(other);
+        return s;
+    }
+
+    static List<Integer> makeList(int[] arr) {
+        List<Integer> b = new ArrayList<>();
+        for (int a: arr)
+            b.add(a);
+        return b;
+    }
 
     static public void main(String[] args) {
-        String[] tickets = {};
-        String[] tickets2;
-        solution(tickets);
+        int[] a = {1, 2, 3, 2};
+        int[] b = {1, 3};
+        int[] a2 = {2,3,4,3,5};
+        int[] b2 = {1,6,7};
+        System.out.println(Arrays.toString(solution(a2, b2)));
     }
 }
