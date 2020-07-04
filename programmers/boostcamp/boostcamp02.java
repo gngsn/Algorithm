c4407d452e60887a64d178dd22ecb7af04a6e0ed
gngsn <rudtjs4540@gmail.com>
2020-07-04 15:47:39 +0900

    Update boostcamp pro

---
+++ programmers/boostcamp/boostcamp02.java
@@ -0,0 +1,16 @@
+package boostcamp;
+
+public class boostcamp02 {
+    static String[] solution(String[] tickets) {
+        String[] answer = {};
+
+        return answer;
+    }
+
+
+    static public void main(String[] args) {
+        String[] tickets = {};
+        String[] tickets2;
+        solution(tickets);
+    }
+}
